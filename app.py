114c1e2cfd8cc7675f6d7dfb16c09364fa03c140
Michael Krajcar <MichaelKrajcar@gmail.com>
2024-10-15 23:36:26 -0700

    deleted then re-added matplotlib and seaborn

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Title of the app, my apologies but I didn't recall any of this in any of the past lessons, so i found examples online to use for this specific page
 st.title("Vehicle Price Analysis by Color")
